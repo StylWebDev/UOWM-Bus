16652c650f2138a76409626337b581af248bd922
AliBaBas02 <kostasstilianou@gmail.com>
2024-10-24 21:32:11 +0300

    Trying PrimeVue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
-import {useConfigureStore} from "../stores/configure.ts";
-import {storeToRefs} from "pinia";
-import Header from "../layouts/Header.vue";
 
-const {test} = storeToRefs(useConfigureStore());
 </script>
 
 <template>
-Home
+
 </template>
 
 <style scoped>
--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -1,9 +1,37 @@
 <script setup lang="ts">
+import Card from "primevue/card";
+import Button from "primevue/button";
 
 </script>
 
 <template>
-Schedule
+<Card
+class="bg-eggplant-950 w-[17vw]"
+>
+  <template #header>
+    <img src="https://astikoktelkastorias.gr/wp-content/uploads/2023/10/mesopotamia-2.png"
+         class="rounded-t-sm bg-red-500"
+         alt="mesopotamia image">
+  </template>
+  <template #title>
+    Μεσοποταμία
+  </template>
+  <template #subtitle>
+    Ζώνη Β
+  </template>
+  <template #content>
+    <p class="m-0">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
+      quas!
+    </p>
+  </template>
+  <template #footer>
+    <div class="flex gap-4 mt-1">
+      <Button label="Cancel" severity="secondary" outlined class="w-full" />
+      <Button label="Save" class="w-full" />
+    </div>
+  </template>
+</Card>
 </template>
 
 <style scoped>
