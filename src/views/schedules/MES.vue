51769a3149c30462fdd86bff281b26b931b19e8b
Styl <kostasstilianou@gmail.com>
2024-10-26 16:59:14 +0300

    Updated Schedules

--- src/views/schedules/MES.vue
+++ src/views/schedules/MES.vue
@@ -20,6 +20,8 @@ const timeTable = [
   "19:30",
   "20:30",
   "21:30",
+  '22:30',
+
 ]
 
 const goTable = [
@@ -57,6 +59,28 @@ const bern2 = computed(() => {
 
   return undefined
 })
+
+const currArrivalOn = computed(() => {
+  return (parseInt(bern.value.slice(3,5)) < new Date().getMinutes()) ? (parseInt(bern2.value.slice(3,5)) - new Date().getMinutes())
+      : (parseInt(bern2.value.slice(3,5)) - (new Date().getMinutes() - parseInt(bern.value.slice(3,5))))
+})
+
+const nextArrivalOn = computed(()=> {
+  return  (parseInt(bern2.value.slice(3,5)) >= new Date().getMinutes())
+      ? ( parseInt(bern2.value.slice(3,5)) - new Date().getMinutes() + parseInt(bern2.value.slice(3,5))) :  ( parseInt(bern2.value.slice(3,5)) + 60 - new Date().getMinutes())
+})
+
+const currArrivalOn2  = computed(() => {
+  return (parseInt(timeTable[ (timeTable.findIndex(value => value === bern.value) === timeTable.length-1) ? 0 : timeTable.findIndex(value => value === bern.value) + 1].slice(3,5)) < new Date().getMinutes())
+      ? ( parseInt(timeTable[ (timeTable.findIndex(value => value === bern.value) === timeTable.length-1) ? 0 : timeTable.findIndex(value => value === bern.value) + 1].slice(3,5)) + 60 - new Date().getMinutes() )
+      :  (parseInt(timeTable[ (timeTable.findIndex(value => value === bern.value) === timeTable.length-1) ? 0 : timeTable.findIndex(value => value === bern.value) + 1].slice(3,5)) - (new Date().getMinutes() - parseInt(bern2.value.slice(3,5))))
+})
+
+const nextArrivalOn2 = computed(() => {
+  return (parseInt(timeTable[ (timeTable.findIndex(value => value === bern.value) === timeTable.length-1) ? 0 : timeTable.findIndex(value => value === bern.value) + 1].slice(3,5)) < new Date().getMinutes())
+      ? ( 60 - new Date().getMinutes() + parseInt(bern2.value.slice(3,5)))
+      :  ( parseInt(bern2.value.slice(3,5)) - new Date().getMinutes() )
+})
 </script>
 
 <template>
@@ -71,9 +95,9 @@ const bern2 = computed(() => {
   </FlexMinified>
 
   <GridMinified v-if=" (bern !== undefined && bern2 !== undefined)" columns="1" lg-columns="3" gap-x="5"
-                class="bg-eggplant-950/85 md:rounded-xl mt-5 md:mx-10 h-fit px-5 pt-4"
+                class="bg-eggplant-950/85 md:rounded-xl mt-5 md:mx-10 px-5 pt-4"
   >
-    <FlexMinified class="h-[80%]"
+    <FlexMinified class=""
         :column="true"
         items="center"
 
@@ -98,16 +122,13 @@ const bern2 = computed(() => {
           <FlexMinified gap-x="4" justify="around" class="bg-emerald-500 py-1.5 ">
             <h5>Τρέχων</h5>
             <p>Ώρα Αναχώρησης: {{bern}}</p>
-            <p>Ώρα Άφιξης {{ (parseInt(bern.slice(3,5)) < new Date().getMinutes()) ? (parseInt(bern2.slice(3,5)) - new Date().getMinutes())
-                : (parseInt(bern2.slice(3,5)) - (new Date().getMinutes() - parseInt(bern.slice(3,5))))}}"</p>
+            <p>Άφιξη σε: {{currArrivalOn }}"</p>
 
           </FlexMinified>
           <FlexMinified gap-x="4" justify="around" class="bg-orange-500 py-1.5 ">
             <h5>Επόμενο</h5>
             <p>Ώρα Αναχώρησης: {{ bern2}}</p>
-            <p>Ώρα Άφιξης: {{
-                (parseInt(bern2.slice(3,5)) >= new Date().getMinutes())
-                    ? ( parseInt(bern2.slice(3,5)) - new Date().getMinutes() + parseInt(bern2.slice(3,5))) :  ( parseInt(bern2.slice(3,5)) + 60 - new Date().getMinutes())}}"
+            <p>Άφιξη σε: {{nextArrivalOn }}"
             </p>
           </FlexMinified>
         </FlexMinified>
@@ -119,48 +140,45 @@ const bern2 = computed(() => {
           <FlexMinified gap-x="4" justify="around" class="bg-emerald-500 py-1.5 ">
             <h5 class="bg-emerald-500">Τρέχων</h5>
             <p>Ώρα Αναχώρησης: {{bern2}}</p>
-            <p>Ώρα Άφιξης {{ (parseInt(timeTable[ (timeTable.findIndex(value => value === bern) === timeTable.length-1) ? 0 : timeTable.findIndex(value => value === bern) + 1].slice(3,5)) < new Date().getMinutes())
-                ? ( parseInt(timeTable[ (timeTable.findIndex(value => value === bern) === timeTable.length-1) ? 0 : timeTable.findIndex(value => value === bern) + 1].slice(3,5)) + 60 - new Date().getMinutes() )
-                :  (parseInt(timeTable[ (timeTable.findIndex(value => value === bern) === timeTable.length-1) ? 0 : timeTable.findIndex(value => value === bern) + 1].slice(3,5)) - (new Date().getMinutes() - parseInt(bern2.slice(3,5)))) }}"</p>
+            <p>Άφιξη σε: {{ currArrivalOn2}}"</p>
 
           </FlexMinified>
 
           <FlexMinified gap-x="4" justify="around" class="mt-4 py-1.5 bg-orange-500">
             <h5>Επόμενο</h5>
             <p>Ώρα Αναχώρησης: {{ timeTable[ (timeTable.findIndex(value => value === bern) === timeTable.length-1) ? 0 : timeTable.findIndex(value => value === bern) + 1]}}</p>
-            <p>Ώρα Άφιξης: {{
-                (parseInt(timeTable[ (timeTable.findIndex(value => value === bern) === timeTable.length-1) ? 0 : timeTable.findIndex(value => value === bern) + 1].slice(3,5)) < new Date().getMinutes())
-                    ? ( 60 - new Date().getMinutes() + parseInt(bern2.slice(3,5)))
-                    :  ( parseInt(bern2.slice(3,5) )- new Date().getMinutes() )}}"</p>
+            <p>Άφιξη σε: {{nextArrivalOn2}}"</p>
           </FlexMinified>
         </FlexMinified>
       </FlexMinified>
 
     </FlexMinified>
 
-    <FlexMinified :column="true" items="center" class=" max-md:h-[40%] md:h-[54%] xl:h-[50%] 2xl:h-[60%] min-[1800px]:h-[65%] min-[2400px]:h-[80%]  w-full lg:col-span-2  ">
+    <FlexMinified :column="true" items="center" class="lg:col-span-2  ">
       <h3 class="text-center  text-xl font-bold text-emerald-500">Πρόγραμμα Λεωφορείου {{`${new Date().getDate()}/${new Date().getMonth()}/${new Date().getFullYear()}`}}</h3>
 
-      <div class="w-full  rounded-lg mt-5 overflow-y-scroll">
-        <table class=" w-full text-sm text-left rtl:text-right text-eggplant-50 ">
-          <thead class="sticky top-0 rounded-t-2xl text-center text-pink-400 text-sm uppercase bg-eggplant-800 ">
-          <tr>
-            <th scope="col" class="px-6 py-3">
+        <FlexMinified :column="true" items="center" class=" w-full text-sm text-left rtl:text-right text-eggplant-50">
+
+          <FlexMinified justify="around" class="sticky w-full top-0 rounded-t-2xl text-center text-pink-400 text-sm uppercase bg-eggplant-800 ">
+            <div class="px-6 py-3">
               ΜΕΤΑΒΑΣΗ
-            </th>
-            <th scope="col" class="px-6 py-3">
+            </div>
+            <div class="px-6 py-3">
               ΕΠΙΣΤΡΟΦΗ
-            </th>
+            </div>
+          </FlexMinified>
 
-          </tr>
-          </thead>
 
-          <tbody v-if="timeTable.length >= goTable.length" class="text-neutral-200 text-center">
-            <tr class="border-b bg-eggplant-500 border-gray-700"
+
+
+          <div v-if="timeTable.length >= goTable.length" class="text-neutral-200 rounded-b-2xl text-center block w-full max-md:h-[35vh] mb-5 md:h-[45vh] lg:h-[36vh] xl:h-[35vh] 2xl:h-[40vh] min-[1800px]:h-[57vh] min-[2200px]:h-[50vh] overflow-y-scroll">
+
+            <FlexMinified class="border-b bg-eggplant-500 border-gray-700"
+                justify="around"
                 v-for="(time,index) in timeTable"
                 :key="index"
             >
-              <td  class="px-6 py-4 font-medium whitespace-nowrap "
+              <div  class="px-6 py-4 font-medium whitespace-nowrap "
                    :class="( parseInt(time.slice(0,2)) === new Date().getHours()
             && parseInt(time.slice(3,5)) <= new Date().getMinutes())
             && parseInt(time.slice(3,5)) >= parseInt(timeTable[(index != undefined) ? index-1 : 0].slice(3,5))
@@ -169,8 +187,9 @@ const bern2 = computed(() => {
                 : null"
               >
               {{time }}
-              </td>
-              <td class="px-6 py-4"
+              </div>
+
+              <div class="px-6 py-4"
                   :class="(
               goTable[index] != undefined
             && parseInt(goTable[index].slice(0,2)) >= new Date().getHours()
@@ -180,41 +199,41 @@ const bern2 = computed(() => {
                 : null"
               >
                 {{(goTable[index] === undefined) ? '-' : goTable[index] }}
-              </td>
-            </tr>
-          </tbody>
-          <tbody v-else class="text-neutral-200 text-center">
-          <tr class="border-b bg-eggplant-500 border-gray-700"
+              </div>
+            </FlexMinified>
+          </div>
+
+          <div v-else class="text-neutral-200 rounded-b-2xl text-center block w-full max-md:h-[35vh] mb-5 md:h-[45vh] lg:h-[36vh] xl:h-[35vh] 2xl:h-[40vh] min-[1800px]:h-[57vh] min-[2200px]:h-[50vh] overflow-y-scroll">
+             <FlexMinified class="border-b w-full bg-eggplant-500 border-gray-700" justify="around"
               v-for="(time,index) in goTable"
               :key="index"
-          >
-            <td  class="px-6 py-4 font-medium whitespace-nowrap"
-            :class="( timeTable[index] != undefined
-            && parseInt(timeTable[index].slice(0,2)) === new Date().getHours()
-            && parseInt(timeTable[index].slice(3,5)) <= new Date().getMinutes())
-            && parseInt(timeTable[index].slice(3,5)) >= parseInt(timeTable[(index != undefined) ? index-1 : 0].slice(3,5))
-            && !(parseInt(bern2.slice(3,5)) <= new Date().getMinutes())
-            ? `bg-emerald-400 text-gray-600 animate-pulse`
-                : null"
-            >
-              {{(timeTable[index] === undefined) ? '-' : timeTable[index] }}
-            </td>
-            <td class="px-6 py-4"
-                style="z-index: 1"
-                :class="( parseInt(time.slice(0,2)) >= new Date().getHours()
-            && parseInt(time.slice(3,5)) <= new Date().getMinutes())
-            && bern2 === time
-            ? `bg-emerald-400 text-gray-600 animate-pulse`
-                : null"
-            >
-              {{time}}
-            </td>
-          </tr>
-          </tbody>
-        </table>
-      </div>
-    </FlexMinified>
+              >
+                <div  class="px-6 py-4 w-full font-medium whitespace-nowrap"
+                      :class="( timeTable[index] != undefined
+                      && parseInt(timeTable[index].slice(0,2)) === new Date().getHours()
+                      && parseInt(timeTable[index].slice(3,5)) <= new Date().getMinutes())
+                      && parseInt(timeTable[index].slice(3,5)) >= parseInt(timeTable[(index != undefined) ? index-1 : 0].slice(3,5))
+                      && !(parseInt(bern2.slice(3,5)) <= new Date().getMinutes())
+                      ?  `bg-emerald-400 text-gray-600 animate-pulse`
+                      : null"
+                >
+                {{(timeTable[index] === undefined) ? '-' : timeTable[index] }}
+                </div>
+                <div class="px-6 py-4 w-full"
+                  style="z-index: 1"
+                  :class="( parseInt(time.slice(0,2)) >= new Date().getHours()
+                  && parseInt(time.slice(3,5)) <= new Date().getMinutes())
+                  && bern2 === time
+                  ? `bg-emerald-400 text-gray-600 animate-pulse`
+                  : null"
+                >
+                {{time}}
+            </div>
+          </FlexMinified>
+          </div>
 
+        </FlexMinified>
+    </FlexMinified>
   </GridMinified>
 
 </template>
